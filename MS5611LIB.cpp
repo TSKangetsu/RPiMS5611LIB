7712804c10e8eba783d7d1547f1cc35d0c9cbe2d
TSKangetsu <x403070567@live.com>
2020-07-18 17:58:10 +0800

    pressure only (tmp commet

---
+++ MS5611LIB.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "MS5611LIB.h"
+
+int main()
+{
+	MS5611 test;
+	test.MS5611Init();
+	test.MS5611PROMSettle();
+	while (true)
+	{
+		test.MS5611PreReader();
+		std::cout << test.Pressure << "\n";
+	}
+}
